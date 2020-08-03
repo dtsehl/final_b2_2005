247622fb341caff7318317b9538cfe0d9dcc0980
Dan Sehl <dan.sehl@gmail.com>
2020-08-03 11:15:46 -0600

    Add instance method tests to flight

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe Flight do
     it {should validate_presence_of :departure_city}
     it {should validate_presence_of :arrival_city}
   end
+
+  describe "Instance Methods" do
+    before :each do
+      @frontier = Airline.create!(name: 'Frontier Airlines')
+      @flight_1 = @frontier.flights.create!(number: "1234", date: "08/01/2020", time: "10:40", departure_city: "Denver", arrival_city: "Las Vegas")
+      @harry = Passenger.create!(name: "Harry", age: 30)
+      @sally = Passenger.create!(name: "Sally", age: 28)
+      @billy = Passenger.create!(name: "Harry", age: 8)
+      @molly = Passenger.create!(name: "Sally", age: 10)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @harry.id, flight_number: @flight_1.number)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @sally.id, flight_number: @flight_1.number)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @billy.id, flight_number: @flight_1.number)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @molly.id, flight_number: @flight_1.number)
+    end
+
+    it ".number_of_minors" do
+      expect(@flight_1.number_of_minors).to eq(2)
+    end
+
+    it ".number_of_adults" do
+      expect(@flight_1.number_of_adults).to eq(2)
+    end
+  end
 end
