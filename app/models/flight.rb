7c2bbbbbacb793a8af025975d8fa24e449663069
Dan Sehl <dan.sehl@gmail.com>
2020-08-03 11:18:31 -0600

    Add uniqueness validation to flight passenger fliht_number & flight nunmber

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,8 @@ class Flight < ApplicationRecord
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
 
-  validates_presence_of :number, :date, :time, :departure_city, :arrival_city
+  validates_presence_of :date, :time, :departure_city, :arrival_city
+  validates_presence_of :number, uniqueness: true
 
   def number_of_minors
     self.passengers.where("age < ?", 18).count
--- app/models/flight_passenger.rb
+++ app/models/flight_passenger.rb
@@ -2,5 +2,5 @@ class FlightPassenger < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
 
-  validates_presence_of :flight_number
+  validates_presence_of :flight_number, uniqueness: true
 end
