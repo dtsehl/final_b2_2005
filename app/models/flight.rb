03d96d53c18f85a1d435498d5525fd81aa263e3e
Dan Sehl <dan.sehl@gmail.com>
2020-08-03 11:12:37 -0600

    Update flight model methods with number_of_minors/adults

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
   has_many :passengers, through: :flight_passengers
 
   validates_presence_of :number, :date, :time, :departure_city, :arrival_city
+
+  def number_of_minors
+    self.passengers.where("age < ?", 18).count
+  end
+
+  def number_of_adults
+    self.passengers.where("age >= ?", 18).count
+  end
 end
