7d4f55a4cdfc42a2411bc5d04887335fd7b760f9
Dan Sehl <dan.sehl@gmail.com>
2020-08-03 10:03:24 -0600

    Create flight show spec

---
+++ spec/features/flights/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Flights Show Page' do
+  describe 'As a Visitor' do
+    before :each do
+      @frontier = Airline.create!(name: 'Frontier Airlines')
+      @flight_1 = @frontier.flights.create!(number: "1234", date: "08/01/2020", time: "10:40", departure_city: "Denver", arrival_city: "Las Vegas")
+      @harry = Passenger.create!(name: "Harry", age: 30)
+      @sally = Passenger.create!(name: "Sally", age: 28)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @harry.id)
+      FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @sally.id)
+    end
+
+    it "I can see the flights details" do
+      visit "/flights/#{@flight_1.id}"
+
+      expect(page).to have_content(@flight_1.number)
+      expect(page).to have_content(@flight_1.date)
+      expect(page).to have_content(@flight_1.time)
+      expect(page).to have_content(@flight_1.departure_city)
+      expect(page).to have_content(@flight_1.arrival_city)
+      expect(page).to have_content(@frontier.name)
+      expect(page).to have_content(@harry.name)
+      expect(page).to have_content(@sally.name)
+    end
+  end
+end
